de0ba82110d231e1ec192733b3d6e1d5d8c1bff2
Jeremy <jlau0012@student.monash.edu>
2018-09-06 00:22:59 +0800

    Code refinement
    Improved documentation, changed to list comprehension.

--- src/graphing_functions.py
+++ src/graphing_functions.py
@@ -10,24 +10,29 @@ from matplotlib import pyplot
 
 def totalContributionChart(dictInput):
     """
-    Displays a pie chart showing percentages of each person's contributuin to
-    a document.
+    Displays a pie chart showing percentages of each person's contribution.
+
+    @:param dictInput: A dictionary with key-value pair of team mamber:
+                       contribution.
+    @:return: None.
+    @:pre-condition:
+    @:post-condition: None.
+    @:complexity: Best and worst case: O(N), where N is the length of input.
     """
     keys = list(dictInput)
-    label = tuple(keys)
-    contribution = []
-
-    for each in keys:
-        contribution.append(dictInput[each])
+    contribution = [dictInput[key] for key in keys] # Creating a list of the
+                                                    # 'value' items from the
+                                                    # input.
 
     fig1, ax1 = pyplot.subplots()
-    ax1.pie(contribution, labels=label, autopct="%1.1f%%")
+    ax1.pie(contribution, labels=keys, autopct="%1.1f%%")
     ax1.axis("equal")
     ax1.set_title("The following is the percentage contribution per person")
     pyplot.show()
 
     return
 
+# Test code.
 if __name__ == "__main__":
     myDict = {"Test 1": 10, "Test 2": 20, "Test 3": 30}
 
