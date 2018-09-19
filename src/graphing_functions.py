e3f69ae8aea0e15236f82647cc36b9d2552a7b84
Jeremy <jlau0012@student.monash.edu>
2018-09-20 00:08:04 +0800

    Graphing functions now return the graph figure.

--- src/graphing_functions.py
+++ src/graphing_functions.py
@@ -5,11 +5,9 @@ Inspector tool.
 Note that these functions make use of the external matplotlib, pyplot, numpy
 and mpld3 libraries.
 """
-#import matplotlib
 from matplotlib import pyplot
 import numpy
 import mpld3
-
 
 def totalContributionChart(dictInput):
     """
@@ -17,27 +15,26 @@ def totalContributionChart(dictInput):
 
     @:param dictInput: A dictionary with key-value pair of team member:
                        contribution.
-    @:return: The figure.
-    @:pre-condition:
+    @:return: A figure object containing the pie chart.
+    @:pre-condition: Input must be a dictionary with numbers as values.
     @:post-condition: None.
     @:complexity: Best and worst case: O(N), where N is the length of input.
     """
     if type(dictInput) != dict:
         raise TypeError
+    
+    names = list(dictInput)
+    contribution = [dictInput[name] for name in names] # Creating a list of the
+                                                       # 'value' items from the
+                                                       # input.
 
-    keys = list(dictInput)
-    contribution = [dictInput[key] for key in keys]  # Creating a list of the
-    # 'value' items from the
-    # input.
-
-    fig1, ax1 = pyplot.subplots()
-    ax1.pie(contribution, labels=keys, autopct="%1.1f%%")
-    ax1.axis("equal")
-    ax1.set_title("The following is the percentage contribution per person")
+    fig, ax = pyplot.subplots()
+    ax.pie(contribution, labels=names, autopct="%1.1f%%")
+    ax.axis("equal")
+    ax.set_title("Percentage contribution per person")
     pyplot.show()
 
-    return fig1
-
+    return fig
 
 def timelineChart(dictInput):
     """
@@ -46,8 +43,9 @@ def timelineChart(dictInput):
     @:param dictInput: A dictionary with key-value pair of team member:
                        contribution, where contribution is also a dictionary
                        with key-value pair of week:contribution.
-    @:return: None.
-    @:pre-condition:
+    @:return: A figure object containing the stacked bar chart.
+    @:pre-condition: Input must be a dictionary with values of dictionaries with
+                     numbers as values.
     @:post-condition: None.
     @:complexity: Best and worst case: O(MN^2), where M is the number of weeks
                   and N is the number of people in the team.
@@ -55,14 +53,19 @@ def timelineChart(dictInput):
     if type(dictInput) != dict:
         raise TypeError
 
+    for key in dictInput:
+        if type(dictInput[key]) != dict:
+            raise TypeError
+
     names = list(dictInput)
     weeks = list(dictInput[names[0]])   # Assume that every person has the same
-    # weeks.
+                                        # weeks.
     # Creating and filling up a contribution matrix.
     contributions = [[dictInput[name][week] for week in weeks]
                      for name in names]
-
+    
     ind = numpy.arange(len(weeks))
+    fig, ax = pyplot.subplots()
     plots = [None for i in range(len(names))]
 
     for person in range(len(contributions)):
@@ -70,25 +73,21 @@ def timelineChart(dictInput):
         for before in range(0, person):
             thisBottom = [thisBottom[week] + contributions[before][week]
                           for week in range(len(thisBottom))]
-        plots[person] = pyplot.bar(ind, contributions[person],
-                                   bottom=thisBottom)
+        plots[person] = ax.bar(ind, contributions[person], bottom = thisBottom)
     pyplot.ylabel("contributions")
     pyplot.title("Contributions per person on a weekly basis")
     pyplot.xticks(ind, weeks)
     pyplot.legend([plots[i][0] for i in range(len(plots))], names)
     pyplot.show()
 
-    return
-
+    return fig
+    
 # Test code.
 if __name__ == "__main__":
     myDict = {"Test 1": 10, "Test 2": 20, "Test 3": 30}
 
-    myPlot = totalContributionChart(myDict)
+    myFig1 = totalContributionChart(myDict)
 
-    # print(mpld3.fig_to_html(myPlot))
+    dict2 = {"Peak Khor":{"2018week36":2,"2018week37":5},"Clare":{"2018week36":5,"2018week37":0}}
 
-    dict2 = {"Peak Khor": {"2018week36": 2, "2018week37": 5},
-             "Clare": {"2018week36": 5, "2018week37": 0}}
-
-    timelineChart(dict2)
+    myFig2 = timelineChart(dict2)
